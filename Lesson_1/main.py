b064bb33d42b489a59bc45188a575c3a6eeee98c
svitlyi <svitlyi_a@itstep.academy>
2024-08-05 19:26:02 +0300

    Init

---
+++ Lesson_1/main.py
@@ -0,0 +1,46 @@
+# print("Hello world!")
+# name = input("Enter your name: ")
+# age = int(input('How old are you? '))
+#
+# print(name, ', ', age)
+# print(f"Name: {name} \nAge: {age}")
+# print(f'{name=}')
+
+# PI = 3.14
+# print(PI)
+#
+# PI = 4
+# if type(PI) is int:
+#     print(PI)
+#
+# if isinstance(PI, int):
+#     print(PI)
+#
+list1 = [123, 'hello', True, 3.14]
+# dict1 = {
+#     'name': 'Andrii',
+#     'age': 25
+# }
+#
+# tuple1 = tuple([123])
+# print(dict1['age'])
+#
+# tuple1[0] = 16
+
+
+#for(int i = 0; i < 10; i++)
+for i in range(10, 0, 1):
+    print(i)
+
+for item in list1:
+    print(item)
+
+a = 0
+while a < 3:
+    print(a)
+    a += 1
+
+while True:
+    # .....
+    if'(....)':
+        break
